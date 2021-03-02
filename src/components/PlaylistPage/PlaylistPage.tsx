a6f326a1bfe6ab0b4c319b43e329735dea0c272c
Klara Hajdu <hajdu.klara.78@gmail.com>
2021-03-02 20:05:45 +0100

    feat: show playlistId for own playlists

--- src/components/PlaylistPage/PlaylistPage.tsx
+++ src/components/PlaylistPage/PlaylistPage.tsx
@@ -6,7 +6,7 @@ import { selectCurrentPlaylist } from "../../store/slices/playlists/selectors";
 import { playlistsAsyncActions } from "../../store/slices/playlists/slice";
 import AddSong from "../AddSong";
 import PlaySongs from "../PlaySongs";
-import { Button, Container, Title } from "./styles";
+import { Button, Container, Subtitle, Title } from "./styles";
 import Songlist from "../Songlist";
 
 const PlaylistPage = () => {
@@ -46,6 +46,7 @@ if (currentPlaylist && currentUser && (currentPlaylist.owner !== currentUser.id)
 return (
         <Container>
             {currentPlaylist && <Title> {currentPlaylist.playlistName} </Title>}
+            {currentPlaylist && <Subtitle> Playlist Id: {currentPlaylist.id} </Subtitle>}
             {currentPlaylist && currentPlaylist.songs && currentPlaylist.songs.length > 0 && <PlaySongs/>}
             <AddSong />
             <Songlist />
