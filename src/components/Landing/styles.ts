6a2b516c314d3acc6b9de6c45295ae8a5fdbd729
Barnabas, Bucsek <bucsek.barnabas@gmail.com>
2021-03-18 12:34:37 +0100

    fix: fix landing page

--- src/components/Landing/styles.ts
+++ src/components/Landing/styles.ts
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 export const Container = styled.div`
     display: flex;
     flex-direction: column;
+    flex: 8.5;
 `
 
 export const Wrapper = styled.div`
@@ -10,7 +11,6 @@ export const Wrapper = styled.div`
     flex-direction: row;
     justify-content: center;
     align-items: center;
-    flex: 8.5;
     height: 100vh;
     flex-wrap: wrap;
 `
